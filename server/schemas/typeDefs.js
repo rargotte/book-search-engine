875fcf6e16524cea4ce96005fb662b4181cd40a3
Regulo Argotte <rargotte@hotmail.com>
2022-05-02 09:53:15 -0500

    initial version  - useCreateIndex and useFindAndModify deleted from connection.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -34,7 +34,7 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(newBook: InputBook!): User
+    saveBook(newBook: SavedBook!): User
     removeBook(bookId: ID!): User
   }
 `;
